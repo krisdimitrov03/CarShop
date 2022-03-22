1e0de4d60c16cb897bca94ed39c16da0bffec97d
Kristian Dimitrov <krisdimitrov03@students.softuni.bg>
2022-03-22 03:32:00 +0200

    Login and Register views and logic added

--- CarShop/Controllers/HomeController.cs
+++ CarShop/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace CarShop.Controllers
 
         public IActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                return View();
+            }
+
             return View();
         }
 
