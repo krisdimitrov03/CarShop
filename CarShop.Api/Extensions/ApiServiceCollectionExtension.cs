cd59a398b8ae43704c7a568fef1535b5f3ab7a5e
Kristian Dimitrov <krisdimitrov03@students.softuni.bg>
2022-03-30 21:25:08 +0300

    Api Added

---
+++ CarShop.Api/Extensions/ApiServiceCollectionExtension.cs
@@ -0,0 +1,25 @@
+﻿using CarShop.Infrastructure.Data;
+using CarShop.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarShop.Api.Extensions
+{
+    public static class ApiServiceCollectionExtension
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            services.AddScoped<IApplicationDbRepository, ApplicationDbRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApiDbContexts(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            return services;
+        }
+    }
+}
